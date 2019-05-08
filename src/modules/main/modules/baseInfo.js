8bec173245083efef4a94cfaa8a1b70d9c47b115
limengke <752140235@qq.com>
2019-05-08 15:25:51 +0800

    feat(main): 加入版本校验功能，提醒升级

--- src/modules/main/modules/baseInfo.js
+++ src/modules/main/modules/baseInfo.js
@@ -1,8 +1,13 @@
 const { Common, Time } = require('../../../util')
 const pkg = require('../../../../package')
+const updateNotifier = require('update-notifier')
 
 exports.baseInfo = () => {
     console.log(Common.getTitle(pkg.name + ' v' + pkg.version, 'red'))
+    const notifier = updateNotifier({pkg});
+    if (notifier.update) {
+        notifier.notify()
+    }
     console.log(Common.getTitle('base'))
     console.log(Time.today)
 }
